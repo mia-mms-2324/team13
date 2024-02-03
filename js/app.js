e4dac46c24358b17b4a5ea1ad25db748ebc98c16
Joey van der Linden <95914820+Jojopro56@users.noreply.github.com>
2024-02-03 23:07:54 +0100

    Ruwe versie apps
    - Nog niet af
    - Gwn om te kijken of mergen nog goed gaat

--- js/app.js
+++ js/app.js
@@ -35,6 +35,12 @@ let mail3Var = document.getElementById('mail3');
 let mail4Var = document.getElementById('mail4');
 let mail5Var = document.getElementById('mail5');
 
+let mailExitCont1 = document.getElementById('mailExitCont1');
+let mailExitCont2 = document.getElementById('mailExitCont2');
+let mailExitCont3 = document.getElementById('mailExitCont3');
+let mailExitCont4 = document.getElementById('mailExitCont4');
+let mailExitCont5 = document.getElementById('mailExitCont5');
+
 let zin1 = document.getElementById('zin1');
 let zin2 = document.getElementById('zin2');
 let zin3 = document.getElementById('zin3');
@@ -396,6 +402,9 @@ function openSystemen() {
   document.querySelector(".desc2").classList.add("ss1");
   document.querySelector(".inbox").classList.add("ss1");
   document.querySelector(".onderzoek").classList.add("ss1");
+
+  document.querySelector(".sysBtns").classList.add("enable");
+  
 }
 
 function closeSystemen() {
@@ -405,6 +414,32 @@ function closeSystemen() {
   document.querySelector(".desc2").classList.remove("ss1");
   document.querySelector(".inbox").classList.remove("ss1");
   document.querySelector(".onderzoek").classList.remove("ss1");
+
+  document.querySelector(".sysBtns").classList.remove("enable");
+}
+
+function openInv() {
+  // Add the logic to open Inv here
+  document.querySelector('.inv').classList.add('open');
+  document.querySelector('.end').classList.add('hide');
+}
+
+function closeInv() {
+  // Add the logic to close Inv here
+  document.querySelector('.inv').classList.remove('open');
+  document.querySelector('.end').classList.remove('hide');
+}
+
+function openEnd() {
+  // Add the logic to open End here
+  document.querySelector('.inv').classList.add('hide');
+  document.querySelector('.end').classList.add('open');
+}
+
+function closeEnd() {
+  // Add the logic to close End here
+  document.querySelector('.inv').classList.remove('hide');
+  document.querySelector('.end').classList.remove('open');
 }
 
 // Function to open Systemen
@@ -415,6 +450,7 @@ function openOnderzoek() {
   document.querySelector(".inbox").classList.add("os1");
   document.querySelector(".onderzoek").classList.add("os1");
   document.querySelector(".zinnen").classList.add("os1");
+  document.querySelector(".titel-zinnen").classList.add("os1");
 
 }
 
@@ -424,6 +460,7 @@ function closeOnderzoek() {
   document.querySelector(".desc3").classList.remove("os1");
   document.querySelector(".inbox").classList.remove("os1");
   document.querySelector(".onderzoek").classList.remove("os1");
+  document.querySelector(".titel-zinnen").classList.remove("os1");
 
   document.querySelector(".zinnen").classList.remove("os1");
 }
@@ -432,6 +469,10 @@ function closeOnderzoek() {
 
 function mail1() {
   document.getElementById('mail1').classList.add('read');
+  document.querySelector("#mail1 .mail-content").classList.add("read");
+  document.querySelector(".inbox").classList.add("read");
+  mailExitCont1.classList.add("read");
+  document.querySelector('.mail-thumbnail').classList.add("read");
 
   document.getElementById('mail2').classList.add('hide');
   document.getElementById('mail3').classList.add('hide');
@@ -441,6 +482,10 @@ function mail1() {
 
 function mail1Exit() {
   document.getElementById('mail1').classList.remove('read');
+  document.querySelector("#mail1 .mail-content").classList.remove("read");
+  document.querySelector(".inbox").classList.remove("read");
+  mailExitCont1.classList.remove("read");
+  document.querySelector('.mail-thumbnail').classList.remove("read");
 
   document.getElementById('mail2').classList.remove('hide');
   document.getElementById('mail3').classList.remove('hide');
@@ -450,6 +495,10 @@ function mail1Exit() {
 
 function mail2() {
   document.getElementById('mail2').classList.add('read');
+  document.querySelector("#mail2 .mail-content").classList.add("read");
+  document.querySelector(".inbox").classList.add("read");
+  mailExitCont2.classList.add("read");
+  document.querySelector('#mail2 .mail-thumbnail').classList.add("read");
 
   document.getElementById('mail1').classList.add('hide');
   document.getElementById('mail3').classList.add('hide');
@@ -459,6 +508,10 @@ function mail2() {
 
 function mail2Exit() {
   document.getElementById('mail2').classList.remove('read');
+  document.querySelector("#mail2 .mail-content").classList.remove("read");
+  document.querySelector(".inbox").classList.remove("read");
+  mailExitCont2.classList.remove("read");
+  document.querySelector('#mail2 .mail-thumbnail').classList.remove("read");
 
   document.getElementById('mail1').classList.remove('hide');
   document.getElementById('mail3').classList.remove('hide');
@@ -466,8 +519,14 @@ function mail2Exit() {
   document.getElementById('mail5').classList.remove('hide');
 }
 
+
+// Function for mail3
 function mail3() {
   document.getElementById('mail3').classList.add('read');
+  document.querySelector("#mail3 .mail-content").classList.add("read");
+  document.querySelector(".inbox").classList.add("read");
+  mailExitCont3.classList.add("read");
+  document.querySelector('#mail3 .mail-thumbnail').classList.add("read");
 
   document.getElementById('mail1').classList.add('hide');
   document.getElementById('mail2').classList.add('hide');
@@ -477,6 +536,10 @@ function mail3() {
 
 function mail3Exit() {
   document.getElementById('mail3').classList.remove('read');
+  document.querySelector("#mail3 .mail-content").classList.remove("read");
+  document.querySelector(".inbox").classList.remove("read");
+  mailExitCont3.classList.remove("read");
+  document.querySelector('#mail3 .mail-thumbnail').classList.remove("read");
 
   document.getElementById('mail1').classList.remove('hide');
   document.getElementById('mail2').classList.remove('hide');
@@ -484,8 +547,13 @@ function mail3Exit() {
   document.getElementById('mail5').classList.remove('hide');
 }
 
+// Function for mail4
 function mail4() {
   document.getElementById('mail4').classList.add('read');
+  document.querySelector("#mail4 .mail-content").classList.add("read");
+  document.querySelector(".inbox").classList.add("read");
+  mailExitCont4.classList.add("read");
+  document.querySelector('#mail4 .mail-thumbnail').classList.add("read");
 
   document.getElementById('mail1').classList.add('hide');
   document.getElementById('mail2').classList.add('hide');
@@ -495,6 +563,10 @@ function mail4() {
 
 function mail4Exit() {
   document.getElementById('mail4').classList.remove('read');
+  document.querySelector("#mail4 .mail-content").classList.remove("read");
+  document.querySelector(".inbox").classList.remove("read");
+  mailExitCont4.classList.remove("read");
+  document.querySelector('#mail4 .mail-thumbnail').classList.remove("read");
 
   document.getElementById('mail1').classList.remove('hide');
   document.getElementById('mail2').classList.remove('hide');
@@ -502,8 +574,13 @@ function mail4Exit() {
   document.getElementById('mail5').classList.remove('hide');
 }
 
+// Function for mail5
 function mail5() {
   document.getElementById('mail5').classList.add('read');
+  document.querySelector("#mail5 .mail-content").classList.add("read");
+  document.querySelector(".inbox").classList.add("read");
+  mailExitCont5.classList.add("read");
+  document.querySelector('#mail5 .mail-thumbnail').classList.add("read");
 
   document.getElementById('mail1').classList.add('hide');
   document.getElementById('mail2').classList.add('hide');
@@ -513,12 +590,17 @@ function mail5() {
 
 function mail5Exit() {
   document.getElementById('mail5').classList.remove('read');
+  document.querySelector("#mail5 .mail-content").classList.remove("read");
+  document.querySelector(".inbox").classList.remove("read");
+  mailExitCont5.classList.remove("read");
+  document.querySelector('#mail5 .mail-thumbnail').classList.remove("read");
 
   document.getElementById('mail1').classList.remove('hide');
   document.getElementById('mail2').classList.remove('hide');
   document.getElementById('mail3').classList.remove('hide');
   document.getElementById('mail4').classList.remove('hide');
 }
+
 
 function zin1Exp() {
   document.querySelector('.zinnen').classList.add('hide');
@@ -582,10 +664,18 @@ function addEventListeners() {
     box2.addEventListener("click", blueHide);
 
     mail1Var.addEventListener('click', mail1);
+    mailExitCont1.addEventListener('click', mail1Exit)
     mail2Var.addEventListener('click', mail2);
+    mailExitCont2.addEventListener('click', mail2Exit)
     mail3Var.addEventListener('click', mail3);
+    mailExitCont3.addEventListener('click', mail3Exit)
     mail4Var.addEventListener('click', mail4);
+    mailExitCont4.addEventListener('click', mail4Exit)
     mail5Var.addEventListener('click', mail5);
+    mailExitCont5.addEventListener('click', mail5Exit)
+
+    document.querySelector('.inv').addEventListener('click', openInv)
+    document.querySelector('.end').addEventListener('click', openEnd)
 
     zin1.addEventListener('click', zin1Exp);
     document.querySelector('.exitCon').addEventListener('click', zin1Exit);
